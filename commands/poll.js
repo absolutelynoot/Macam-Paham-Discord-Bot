ad10796b9c3d4e61a827fc214c00fbc049842e07
absolutelynoot <isalsamudra@hotmail.com>
2020-12-25 21:02:46 +0800

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -6,19 +6,16 @@ module.exports = {
         .setColor('#F3CD98')
         .setTitle('Initiate Poll')
         .setDescription("!poll to initiate a simple yes or no poll")
-        .setTimestamp()
         if(!args[1]){
             message.channel.send(newEmbed)
+        } else {
+            let msgArgs = args.slice(1).join(" ")
+            message.channel.send("📋" + "**" msgArgs + "**").then(messageReaction => {
+                messageReaction.react("👍");
+                messageReaction.react("👎");
+                message.delete(1000).catch(console.error)
+            });
         }
-
-        let msgArgs = args.slice(1).join(" ")
-
-        message.channel.send("📋**" + msgArgs + "**").then(messageReaction => {
-            messageReaction.react("👍");
-            messageReaction.react("👎");
-            message.delete(1000).catch(console.error)
-        });
-
         
     }
 }
