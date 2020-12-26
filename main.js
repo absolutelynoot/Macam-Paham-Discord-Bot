9457ac29a070a23e772312d8c124ba8805b552b1
absolutelynoot <isalsamudra@hotmail.com>
2020-12-26 13:19:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -31,9 +31,8 @@ client.on('guildMemberAdd', guildMember =>{
         .setDescription('Thank you for joining us! Good luck and have fun!')
         .setThumbnail('https://scontent.fsin9-1.fna.fbcdn.net/v/t1.0-9/132798110_101959391833061_3898216952922430083_n.jpg?_nc_cat=111&ccb=2&_nc_sid=730e14&_nc_ohc=eb8KfxrJ_wsAX-Hws_l&_nc_oc=AQmcmiJmmEK8UOBd3h6rZA-Ze77Er5rDtHjslq5160-e9E1ShVgTN4jVYg2H0lK8hqw&_nc_ht=scontent.fsin9-1.fna&oh=86dbc7d388a41a0ae65f471e48f50031&oe=6008F5D1')
         .addFields(
-            {name: 'Introduction', value: "For those who didn't know, the word 'Macam Paham' is a Malay phrase and a common Singaporean Slang which directly translates to 'like you know it all'."},
-            {name: '\u200B', value: '\u200B' },
-            {name: 'Channels', value: "💬chat - General chat, talk about anything\n🏆gg-wins - Brag your victory, clips and highlights\n📰news - Latest updates and patch notes on games"},
+            {name: 'Introduction', value: "For those who didn't know, the word 'Macam Paham' is a Malay phrase and a common Singaporean Slang which directly translates to 'like you know it all'.\n"},
+            {name: 'Channels', value: "💬chat - General chat, talk about anything\n🏆gg-wins - Brag your victory, clips and highlights\n📰news - Latest updates and patch notes on games\n"},
             {name: '\u200B', value: '\u200B' },
             {name: 'Server Rules', value: 'type `!rules` to see the server rules'},
             {name: 'Roles and Rewards', value: 'type `!roles` to see the server roles and rewards'},
